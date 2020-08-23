3b03f24b3736f464a8c573ed4cc1c6c325f4192b
Park Shin Young <qkrtlsdud428@gmail.com>
2020-08-23 17:37:36 +0900

    Update permu.cpp

--- algorithm/c++/순열과조합/permu.cpp
+++ algorithm/c++/순열과조합/permu.cpp
@@ -1,12 +1,12 @@
 //순열
-//4P2  4중 2개를 선택하여 나열
+//5P3 5중 3개를 선택하여 나열
 #include <iostream>
 
 using namespace std;
-int k = 2;
-int n = 4;
-int arr[2];
-bool check[4];
+int k = 3;
+int n = 5;
+int arr[3];
+bool check[5];
 void getResult(int inx) {
 	if (inx >= k) {
 		for (int i = 0; i < k; i++) {
@@ -19,9 +19,9 @@ void getResult(int inx) {
 		if (check[i])
 			continue;
 		arr[inx] = i;
-		check[inx] = true;
+		check[i] = true;
 		getResult(inx + 1);
-		check[inx] = false;
+		check[i] = false;
 	}
 }
 int main() {
