0c4f69bbeb89bf7a9cec88c2ec3e66f86e7c26e3
Park Shin Young <qkrtlsdud428@gmail.com>
2020-08-24 19:13:26 +0900

    Update binary_search.py

--- algorithm/python/binary_search.py
+++ algorithm/python/binary_search.py
@@ -1,19 +1,28 @@
 def binary_search(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
+        #중간점 인덱스 반환
+        
         if array[mid] == target:
             return mid
+        #중간점 값보다 찾고자 하는 값이 클 경우 오른쪽 확인
         elif array[mid] < target:
             start = mid + 1
+        #중간점 값보다 찾고자 하는 값이 작을 경우 왼쪽 확인
         else:
             end = mid - 1
 
-    return None
+    return None #못찾을 경우
 
 
 if __name__ == "__main__":
+    #입력받기
+    #원소의 개수와 찾고자하는 값
     n, target = list(map(int, input().split()))
+    #전체원소 입력받기
     array = list(map(int, input().split()))
+
+    #이진탐색 수행 결과 출력
     result = binary_search(array, target, 0, n-1)
     if result == None:
         print("원소가 존재하지 않습니다.")
