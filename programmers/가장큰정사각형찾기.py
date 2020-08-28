fff7c7609ea86dbe5430ab1d73676221ea7d6892
Park Shin Young <qkrtlsdud428@gmail.com>
2020-08-28 16:17:48 +0900

    Update 가장큰정사각형찾기.py

--- programmers/가장큰정사각형찾기.py
+++ programmers/가장큰정사각형찾기.py
@@ -1,10 +1,8 @@
 def solution(board):
-    answer = 1
     width = len(board[0])
     height = len(board)
     for i in range(1, height):
         for j in range(1, width):
             if board[i][j] == 1:
                 board[i][j] = min(board[i-1][j-1], min(board[i-1][j], board[i][j-1])) + 1
-                answer = max(answer, board[i][j])
-    return answer ** 2
+    return max([item for row in board for item in row]) ** 2
