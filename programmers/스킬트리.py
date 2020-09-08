350b7f7f04cf95e18bfc06d8d09dc3a022b99c60
shin0park <38244852+shin0park@users.noreply.github.com>
2020-09-09 00:47:18 +0900

    Create 스킬트리.py

---
+++ programmers/스킬트리.py
@@ -0,0 +1,16 @@
+#첫번째 풀이
+def solution(skill, skill_trees):
+    answer = 0
+    for skill_tree in skill_trees:
+        is_vaild = True
+        pre = -1
+        for st in skill_tree:
+            if st in skill:
+                now = skill.index(st)
+                if pre > now or now != pre + 1 or (pre == -1 and now != 0):
+                    is_vaild = False
+                    break
+                pre = now
+        if is_vaild:
+            answer += 1
+    return answer
