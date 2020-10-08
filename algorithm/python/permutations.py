4d45ada9e401448aa51e1ccf1accb8da44f328b0
shin0park <38244852+shin0park@users.noreply.github.com>
2020-10-08 21:09:54 +0900

    Create permutations.py

---
+++ algorithm/python/permutations.py
@@ -0,0 +1,15 @@
+'''
+[1,2,3,4] 2개 순열
+'''
+data = [1,2,3,4]
+k = 2
+def my_permutaion(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for next in my_permutaion(arr[:i] + arr[i+1:], r-1):
+                yield [arr[i]] + next
+
+for per in my_permutaion(data, k):
+    print(per)
