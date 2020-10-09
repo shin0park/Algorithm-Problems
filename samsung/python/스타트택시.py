3056eefd0ea34f9c52919d0335750c0cca551655
shin0park <38244852+shin0park@users.noreply.github.com>
2020-10-09 14:36:19 +0900

    Update 스타트택시.py

--- samsung/python/스타트택시.py
+++ samsung/python/스타트택시.py
@@ -2,6 +2,7 @@
 스타트 택시
 '''
 from collections import deque
+
 n, m, fuel = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 people = [[0] * n for _ in range(n)]
@@ -10,15 +11,16 @@ dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
 startX -= 1
 startY -= 1
-dest = [[0,0]]
+dest = [[0, 0]]
 for i in range(m):
     psx, psy, pdx, pdy = map(int, input().split())
     people[psx - 1][psy - 1] = i + 1
     dest.append([pdx - 1, pdy - 1])
 
+#가장 가까운 승객 찾기
 def find_people(sx, sy):
     result = []
-    dist = [[-1]*n for _ in range(n)]
+    dist = [[-1] * n for _ in range(n)]
     q = deque([])
     q.append([sx, sy])
     dist[sx][sy] = 0
@@ -33,19 +35,24 @@ def find_people(sx, sy):
             nx, ny = x + dx[i], y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= n:
                 continue
-            if dist[nx][ny] != -1 or board[nx][ny] == 1: #방문 or 벽
+            # 방문 or 벽
+            if dist[nx][ny] != -1 or board[nx][ny] == 1:
                 continue
             dist[nx][ny] = dist[x][y] + 1
-            q.append([nx,ny])
+            q.append([nx, ny])
+    # 같은 최단거리인 승객 추가
     while q:
         x, y = q.popleft()
         if people[x][y] != 0 and dist[x][y] == result_dist:
             result.append([x, y, people[x][y], dist[x][y]])
     result.sort()
+    # 추가된 승 중 우선순위 높은 승객
     if not result:
-        return [0,0,0,0]
+        return [0, 0, 0, 0]
     else:
         return result[0]
+
+#승객을 목적지로 이동
 def moving(sx, sy, dest_spot):
     global fuel
     dist = [[-1] * n for _ in range(n)]
@@ -54,7 +61,7 @@ def moving(sx, sy, dest_spot):
     dist[sx][sy] = 0
     while q:
         x, y = q.popleft()
-        if fuel >= dist[x][y] and [x,y] == dest_spot:
+        if fuel >= dist[x][y] and [x, y] == dest_spot:
             fuel += dist[x][y]
             return True
         for i in range(4):
@@ -65,8 +72,9 @@ def moving(sx, sy, dest_spot):
             if dist[nx][ny] != -1 or board[nx][ny] == 1:
                 continue
             dist[nx][ny] = dist[x][y] + 1
-            q.append([nx,ny])
+            q.append([nx, ny])
     return False
+
 
 while m:
     rideX, rideY, people_num, moving_len = find_people(startX, startY)
