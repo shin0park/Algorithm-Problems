31e9648d4d009cf145b54b972ed3f3f560ecaa0d
shin0park <38244852+shin0park@users.noreply.github.com>
2020-10-14 15:32:54 +0900

    Update permutations_product.py

--- algorithm/python/permutations_product.py
+++ algorithm/python/permutations_product.py
@@ -1,15 +1,18 @@
 '''
 [1,2,3,4] 2개 순열
 '''
-data = [1,2,3,4]
+data = [1, 2, 3, 4]
 k = 2
+
+
 def my_permutaion(arr, r):
     for i in range(len(arr)):
         if r == 1:
             yield [arr[i]]
         else:
-            for next in my_permutaion(arr, r-1):
+            for next in my_permutaion(arr, r - 1):
                 yield [arr[i]] + next
+
 
 for per in my_permutaion(data, k):
     print(per)
